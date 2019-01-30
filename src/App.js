328ca92fdd3dd193b82a0edd8ce966d802e4e463
Elias Flores <eliasflores13@outlook.com>
2019-01-30 11:23:07 -0400

    Added the complete Todo button to complete a Todo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([
-    {text: "Learn about React"},
-    {text: "Meet friend for lunch"},
-    {text: "Build really cool todo app"}
+    {text: "Learn about React", isCompleted: false},
+    {text: "Meet friend for lunch", isCompleted: false},
+    {text: "Build really cool todo app", isCompleted: false}
   ]);
 
   const addTodo = text => {
@@ -16,11 +16,17 @@ function App() {
     setTodos(newTodos);
   };
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo}/>
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
